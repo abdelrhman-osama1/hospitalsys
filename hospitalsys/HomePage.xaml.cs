77146af2fab17da427f1051fec502b48cbfc6853
abdelrhmanOsama <abdoosama010@gmail.com>
2025-08-06 08:50:22 +0300

    adding patient and data

--- hospitalsys/HomePage.xaml.cs
+++ hospitalsys/HomePage.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using DatabaseProject;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,9 +21,26 @@ namespace hospitalsys
     /// </summary>
     public partial class HomePage : Window
     {
+        private void LoadPatients()
+        {
+            DBAccess db = new DBAccess();
+            DataTable dt = new DataTable();
+            String query = "SELECT ID, Name, Phone, Age, Address, Nationality, Gender, Insurance FROM Patients";
+            db.readDatathroughAdapter(query, dt);
+            PatientData.ItemsSource = dt.DefaultView;
+        }
         public HomePage()
         {
             InitializeComponent();
+            LoadPatients();
+        }
+
+        private void AddBtn_Click(object sender, RoutedEventArgs e)
+        {
+            PatientFormWIndow wIndow = new
+                PatientFormWIndow();
+            wIndow.ShowDialog();
+            LoadPatients();
         }
     }
 }
---
+++ hospitalsys/PatientFormWIndow.xaml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using DatabaseProject;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace hospitalsys
+{
+    /// <summary>
+    /// Interaction logic for PatientFormWIndow.xaml
+    /// </summary>
+    public partial class PatientFormWIndow : Window
+    {
+        public PatientFormWIndow()
+        {
+            InitializeComponent();
+        }
+
+        private void SaveNew_Click(object sender, RoutedEventArgs e)
+        {
+            String name = PatientName.Text;
+            String Phone = PatientNumber.Text;
+            String Age = PatientAge.Text;
+            String Address = PatientAddress.Text;
+            String Nationality = PatientNationality.Text;
+            String Gender = (PatientGender.SelectedItem as ComboBoxItem)?.Content?.ToString();
+            String insurance = (PatientInsurance.SelectedItem as ComboBoxItem)?.Content?.ToString() ?? "None" ;
+
+            if (String.IsNullOrWhiteSpace(name))
+            {
+                MessageBox.Show("Pleas enter Patient name" ,"Warnning", MessageBoxButton.OK, MessageBoxImage.Warning);
+                return;
+            }
+            try
+            {
+                DBAccess db = new DBAccess();
+                db.createConn();
+
+                string insertQuery = "INSERT INTO patients (NAME, Phone, Age, Address, Nationality, Gender, insurance) VALUES (@Name, @phone, @Age, @Address, @Nationality, @Gender, @insurance)";
+                SqlCommand cmd = new
+                    SqlCommand(insertQuery);
+                cmd.Parameters.AddWithValue("@Name", name);
+                cmd.Parameters.AddWithValue("@Phone", Phone);
+                cmd.Parameters.AddWithValue("@Age", Age);
+                cmd.Parameters.AddWithValue("@Address", Address);
+                cmd.Parameters.AddWithValue("@Nationality", Nationality);
+                cmd.Parameters.AddWithValue("@Gender", Gender);
+                cmd.Parameters.AddWithValue("@insurance", insurance);
+                db.executeQuery(cmd);
+                MessageBox.Show("Patient added ", "Done", MessageBoxButton.OK, MessageBoxImage.Information);
+                db.closeConn();
+                this.Close();
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error occur: " + ex.Message, "error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void PatientNumber_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            e.Handled = !e.Text.All(char.IsDigit);
+        }
+    }
+}
